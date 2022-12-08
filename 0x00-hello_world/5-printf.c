159c9e48a45f2ee9226ae625b6e765bfebcb4c3a
Okafor Nelson <okafornel@gmail.com>
2022-12-08 06:27:20 -0800

    Create 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of all C program
+ * @void: Doesn't take any parameters
+ *
+ * Return: It returns zero upon successful run of program
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
