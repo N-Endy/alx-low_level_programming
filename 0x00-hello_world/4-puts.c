9ef756660b62f49d47de7a55884071230cea4b22
Okafor Nelson <okafornel@gmail.com>
2022-12-08 06:06:48 -0800

    Change commands to C syntax removing script mode

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-$!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/*
+ * main - entry point of the program
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
