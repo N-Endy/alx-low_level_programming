111f80419672eedcac7835f4a862305417af0712
Okafor Nelson <okafornel@gmail.com>
2022-12-09 07:34:36 -0800

    Change to int

--- 0x01-variables_if_else_while/test.c
+++ 0x01-variables_if_else_while/test.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		if (i != '9')
+		if (i < 57)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
