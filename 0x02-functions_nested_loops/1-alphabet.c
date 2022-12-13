9dd1bef04e3901b616dad4af6551bedb3148cf49
Okafor Nelson <okafornel@gmail.com>
2022-12-13 03:45:10 -0800

    Change function name from main to print_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point for a C programs
+ * print_alphabet - Print letters of the alphabet
  *
- * Return: Returns 0 for a successful program
+ * Description: The alphabet printed are all in lowercase
+ * Return: No return value;
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-
-	return (0);
 }
