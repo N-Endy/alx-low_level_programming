20fc0e50f69be615a9a57d92adb522c4fc0a0bd0
Okafor Nelson <okafornel@gmail.com>
2022-12-13 06:32:40 -0800

    Create 6-abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the absolute value of an integer
+ * @c: Takes in an integer as argument
+ * Return: Returns an integer value
+ */
+
+int _abs(int c)
+{
+	c < 0 ? c * -1 : c;
+}
