0efb0fbb9d4c40f15a2bffb98e4a4d054d7db114
Okafor Nelson <okafornel@gmail.com>
2022-12-13 03:21:06 -0800

    Create 0-putchar.c file with corresponding code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point for all C language program
+ *
+ * Description: prints "_putchar", followed by a new line
+ * Return: Returns 0 on successful completion of program
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
