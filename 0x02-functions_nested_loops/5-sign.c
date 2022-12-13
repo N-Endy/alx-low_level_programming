ad19358da99f44ae262d49e30ba59fc3ed55da2e
Okafor Nelson <okafornel@gmail.com>
2022-12-13 06:17:42 -0800

    Change to signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,13 +12,13 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
-		_putchar('1');
+		_putchar('+');
 		return (1);
 	} else if (c < 0)
 	{
-		_putchar('-1');
+		_putchar('-');
 		return (-1);
 	}
-	_putchar('0');
+	_putchar('48');
 	return (0);
 }
