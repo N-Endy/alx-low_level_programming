5eae22060b576bb9049bb925a0e5229aa7c69a8b
Okafor Nelson <okafornel@gmail.com>
2022-12-15 04:47:33 -0800

    Create 1-isdigit file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit (through 0 - 9)
+ * @c: integer value argument
+ *
+ * Return: Returns 1 for true and 0 for false
+ */
+
+int _isdigit(int c)
+{
+	return (c > 47 && c < 58);
+}
