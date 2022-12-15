1d81d29468022711be1a94c920dd2559b7023370
Okafor Nelson <okafornel@gmail.com>
2022-12-15 04:53:39 -0800

    Create 2-mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integer
+ * @a: First integer value
+ * @b: Second integer value
+ *
+ * Return: Returns an integer value of the product of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
