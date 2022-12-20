df9d8739248de13c2a9facdee59586c906c0697f
Okafor Nelson <okafornel@gmail.com>
2022-12-20 02:33:26 -0800

    Add two loops

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 	int count = 0;
 	int i = 0;
 	int j, k;
-	char res[];
+	char res;
 
 	/* Get the length of the string */
 	while (s[i] != '\0')
@@ -23,5 +23,9 @@ void rev_string(char *s)
 	/* Use length to print starting from back */
 	for (j = count - 1; j >= 0; j--)
 		for (k = 0; k <= count - 1; k++)
-			*(res + k) = *s[j];
+		{
+			res = s[k];
+			s[k] = s[j];
+			s[j] = res;
+		}
 }
