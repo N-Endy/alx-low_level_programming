0de71f8aab02eb3a992dbd17af7ce5442d9b18d0
Okafor Nelson <okafornel@gmail.com>
2022-12-22 01:25:11 -0800

    Create 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two string
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: returns 0 if strings are equal, negative integer if
+ * char at s1 is less than char at s2, and positive otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, res;
+
+	/* Compare both strings */
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		/* return difference if they differ */
+		if (s1[i] != s2[i])
+		{
+			res = s1[i] - s2[i];
+			break;
+		} else
+			res = s1[i] - s2[i];
+		i++;
+	}
+
+	return (res);
+}
