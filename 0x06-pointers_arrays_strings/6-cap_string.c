3c763639465b4221551aa0ea3876652d1d88ec6e
Okafor Nelson <okafornel@gmail.com>
2022-12-22 03:20:21 -0800

    Add condition check for first word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,11 +8,14 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0, j;
+	int i, j;
 	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40,
 		41, 123, 125};
 
-	while (str[i] != '\0')
+	if (str[0] > 96 && str[0] < 123)
+		str[0] = str[0] - 32;
+
+	for (i = 0; str[i] != '\0', i++)
 	{
 		for (j = 0; j < 13; j++)
 		{
@@ -23,7 +26,6 @@ char *cap_string(char *str)
 				str[i] = str[i] - 32;
 			}
 		}
-		i++;
 	}
 
 	return (str);
