b0c68d14d42634708e0c707ceb315a8a363a07f0
Okafor Nelson <okafornel@gmail.com>
2022-12-22 05:13:53 -0800

    Add * to all str

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -12,18 +12,18 @@ char *cap_string(char *str)
 	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40,
 		41, 123, 125};
 
-	if (*str[0] > 96 && *str[0] < 123)
-		*str[0] = *str[0] - 32;
+	if (str[0] > 96 && str[0] < 123)
+		str[0] = str[0] - 32;
 
-	for (i = 1; *str[i] != '\0'; i++)
+	for (i = 1; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 13; j++)
 		{
-			if (*str[i] == sep[j])
+			if (str[i] == sep[j])
 			{
 				/* Checks if char is a lowercase */
-				if (*str[i] > 96 && *str[i] < 123)
-				*str[i] = *str[i] - 32;
+				if (str[i] > 96 && str[i] < 123)
+				str[i] = str[i] - 32;
 				break;
 			}
 		}
