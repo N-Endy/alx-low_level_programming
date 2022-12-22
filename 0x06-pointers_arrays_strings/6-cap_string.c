d667ad7064b6deabb2d66552ab6c574d535d0431
Okafor Nelson <okafornel@gmail.com>
2022-12-22 03:24:36 -0800

    Remove first check befor if

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,6 @@ char *cap_string(char *str)
 	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40,
 		41, 123, 125};
 
-	if (str[0] > 96 && str[0] < 123)
-		str[0] = str[0] - 32;
-
 	for (i = 0; str[i] != '\0', i++)
 	{
 		for (j = 0; j < 13; j++)
