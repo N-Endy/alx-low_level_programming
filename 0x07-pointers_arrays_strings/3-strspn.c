cec5d367976dd312a2b0d77329e580f8d9f7a773
Okafor Nelson <okafornel@gmail.com>
2023-01-02 11:04:06 -0800

    Create 3 strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: String to search
+ * @accept: Characters s must contain
+ *
+ * Return: the legth of the prefix substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (accept[j] == s[i])
+				len++;
+			break;
+
+	return (len);
+}
