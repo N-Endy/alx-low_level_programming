fa67b98c35cdfd1cf8cb16454b076a61ad16a661
Okafor Nelson <okafornel@gmail.com>
2023-01-04 11:37:46 -0800

    Create 2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Gives the length of a string
+ * @s: The string to act on
+ *
+ * Return: Returns the length of the string, int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int res = 0;
+
+	if (*s)
+	{
+		res++;
+		_strlen_recursion(s + 1);
+	}
+}
