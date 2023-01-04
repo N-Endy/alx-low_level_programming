9abf0989bd0632d2afee16f8d23e644e233559bb
Okafor Nelson <okafornel@gmail.com>
2023-01-04 14:59:24 -0800

    Adjust code betty style

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,22 +10,22 @@
 
 int wildcmp(char *s1, char *s2)
 {
-    /* base case: if both strings are empty, they are equal */
-    if (*s1 == '\0' && *s2 == '\0')
-        return 1;
+	/* base case: if both strings are empty, they are equal */
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-    /* if s2 is empty, s1 must be empty for strings to be equal */
-    if (*s2 == '\0')
-        return 0;
+	/* if s2 is empty, s1 must be empty for equal strings */
+	if (*s2 == '\0')
+		return (0);
 
-    /* if s2 is '*', recursively check the rest of s1 and s2 */
-    if (*s2 == '*')
-        return wildcmp(s1, s2+1) || wildcmp(s1+1, s2);
+	/* if s2 is '*', recursively check the rest of s1 and s2 */
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
-    /* if the characters are not equal, return 0 */
-    if (*s1 != *s2)
-        return 0;
+	/* if the characters are not equal, return 0 */
+	if (*s1 != *s2)
+		return (0);
 
-    /* otherwise, recursively check the rest of the strings */
-    return wildcmp(s1+1, s2+1);
+	/* otherwise, recursively check the rest of the strings */
+	return (wildcmp(s1 + 1, s2 + 1));
 }
