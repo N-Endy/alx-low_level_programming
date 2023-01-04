e9931f6c2879c6635d4972d77d8b7877281668d7
Okafor Nelson <okafornel@gmail.com>
2023-01-04 11:47:42 -0800

    Refractor code

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,16 +9,7 @@
 
 int _strlen_recursion(char *s)
 {
-	int res = 0;
-
 	if (*s == '\0')
 		return (0);
-
-	if (*s)
-	{
-		res++;
-		_strlen_recursion(s + 1);
-	}
-
-	return (res);
+	return (1 + _strlen_recursion(s + 1);
 }
