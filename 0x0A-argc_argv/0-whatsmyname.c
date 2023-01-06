3b16cc4ac3f328c62e69dfc73ed8e44bc8ba2b7e
Okafor Nelson <okafornel@gmail.com>
2023-01-06 12:25:01 -0800

    Add attribute unused

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
  * @argc: Number of arguments
  * @argv: Array of strings for arguments
  *
- * Return: Return name
+ * Return: Return 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
