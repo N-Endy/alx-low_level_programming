bb73a800dc3678d472908543176554465c4edb01
Okafor Nelson <okafornel@gmail.com>
2023-01-06 12:20:00 -0800

    Create task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name, followed by a new line
+ * @argc: Number of arguments
+ * @argv: Array of strings for arguments
+ *
+ * Return: Return name
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# C - argc, argv
+
+ALX project to show knowledge of how to manipulate the main variables, argc and argv
