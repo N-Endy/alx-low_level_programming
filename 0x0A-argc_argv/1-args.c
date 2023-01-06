825600901d8246bbe4b13fc5dd4e4adb78ab9143
Okafor Nelson <okafornel@gmail.com>
2023-01-06 12:28:16 -0800

    Create task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments followed by new line
+ * @argc: Number of arguments
+ * @argv: Array of strings for arguments
+ *
+ * Return: Return name
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
