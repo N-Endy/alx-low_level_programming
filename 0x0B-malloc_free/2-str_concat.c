849c896412ae74c5070324884d0518e4a5446431
Okafor Nelson <okafornel@gmail.com>
2023-01-10 08:50:13 -0800

    Create task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Returns the concatenated version of both strings
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len;
+	char *_concat;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	/* get length of strings */
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
+	len = i + j;
+
+	_concat = malloc(sizeof(char) * len);
+	if (_concat == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		_concat[i] = s1[i];
+	for (j = 0; s2[j]; j++)
+		_concat[j + i] = s2[j];
+
+	return (_concat);
+}
