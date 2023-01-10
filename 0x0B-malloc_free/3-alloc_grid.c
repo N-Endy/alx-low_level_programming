e86ec24b33e4ed81cac1137a6daf6ebf4577ecfe
Okafor Nelson <okafornel@gmail.com>
2023-01-10 13:11:47 -0800

    Change to **arr

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	int *arr;
+	int **arr;
 
 	arr = malloc(sizeof(int) * width * height);
 	if (arr == NULL)
