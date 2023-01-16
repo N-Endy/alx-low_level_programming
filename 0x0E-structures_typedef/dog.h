c7b6efce8cfcd51959a6ea3dc7255921f8fd74fb
Okafor Nelson <okafornel@gmail.com>
2023-01-16 12:39:47 -0800

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Short description
+ * @name: First member type char
+ * @age: Second member type float
+ * @owner: Third member type char
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
