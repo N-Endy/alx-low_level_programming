ce1a853f76ddf198e8878ffa219a911c0e466822
Okafor Nelson <okafornel@gmail.com>
2023-01-17 03:36:59 -0800

    Create task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Function that frees dog structs
+ * @d: Pointer to a struct
+ *
+ * Return: Void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	d->name = NULL;
+	free(d->owner);
+	d->owner = NULL;
+	free(d);
+	d = NULL;
+}
