c8e3bd618fb899beafb34375df184aa927c7b9b9
Okafor Nelson <okafornel@gmail.com>
2023-01-18 11:08:15 -0800

    Create task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ * @name: A person's name
+ * @f: Pointer to a function
+ *
+ * Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != "" && f != NULL)
+		f(name);
+}
