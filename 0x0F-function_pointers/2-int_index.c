9cc09b17483db66eb877ff39f0d411fe2ff5f3d1
Okafor Nelson <okafornel@gmail.com>
2023-01-18 11:51:32 -0800

    Add if statement to check array and cmp exist

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,12 +15,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		ele = cmp(array[i])
-		if (ele == 0)
-			break;
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			ele = cmp(array[i])
+			if (ele == 0)
+				break;
+				return (i);
+		}
 	}
 	return (-1);
 }
