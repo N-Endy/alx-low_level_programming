6ca343a9767836b8e5b065499f9a941f5f827a5d
Okafor Nelson <okafornel@gmail.com>
2023-01-18 12:04:32 -0800

    Code

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -21,9 +21,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			ele = cmp(array[i])
 			if (ele == 0)
+			{
 				break;
+				return (i);
+			}
 		}
-		return (i);
 	}
 	return (-1);
 }
