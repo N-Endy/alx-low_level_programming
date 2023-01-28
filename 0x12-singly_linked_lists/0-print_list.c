a7eed5f270f7843c98d4548fb0af161ebbcad9cf
Okafor Nelson <okafornel@gmail.com>
2023-01-28 09:46:43 -0800

    Initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: list to print
+ *
+ * Return: Returns a list of element in the node
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	list_t node = h;
+	
+	// traseverse nodes
+	while (node != NULL)
+	{
+		if (node->str)
+			printf("%u %s\n", node->len, node->str);
+		else
+			printf("%u %s\n", 0, "(nil)");
+		n++;
+		node = node->next;
+	}
+
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# Singly Linked Lists
+
+This directory contains projects showing knowledge of using singly linked list in C Language.
