48bb7aeb3f0d9776384ce65ed416ee2c2ad95221
Okafor Nelson <okafornel@gmail.com>
2023-01-29 01:23:51 -0800

    Create task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* list_len - prints all the elements of a list_t list
+* @h: pointer to the start of the list
+*
+* Return: number of elements in the list
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *temp;
+
+	temp = h;
+	if (temp == NULL)
+		return (0);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
