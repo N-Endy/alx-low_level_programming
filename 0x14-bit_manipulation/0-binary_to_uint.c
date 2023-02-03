0417e3b215aec7beb0a446e7ca0480fe91ccfc73
Okafor Nelson <okafornel@gmail.com>
2023-02-03 03:37:00 -0800

    Add last }

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int
@@ -35,4 +33,4 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	return (result);
-
+}
